cd44387a63d6d3ff231dd1a6f0d6a9e6b308c52b
Roman Karpov <roman.karpov@gmail.com>
2023-04-06 09:27:12 +0300

    webcomponents

---
+++ app/TestAppComponent.js
@@ -0,0 +1,25 @@
+import Component, {html} from '../src/Component.js';
+import TestSettingsComponent from './TestSettingsComponent.js';
+
+customElements.define('test-settings', TestSettingsComponent);
+
+export default class TestAppComponent extends Component(HTMLElement) {
+  render () {
+    return html`
+      <style>
+        a {color: red;}
+      </style>
+      <div>
+        <h4>
+          <a href="#/${this.model.id}">
+            <span property="id"></span>
+          </a>
+        </h4>
+        <ul property="rdfs:label"><li></li></ul>
+        <div rel="v-s:hasSettings">
+          <test-settings></test-settings>
+        </div>
+      </div>
+    `;
+  }
+}
---
+++ app/TestSettingsComponent.js
@@ -0,0 +1,22 @@
+import Component, {html} from '../src/Component.js';
+
+export default class TestSettingsComponent extends Component(HTMLElement) {
+  pre (root) {
+    root.append('!!!');
+  }
+
+  post (root) {
+    root.append('>>>');
+  }
+
+  render () {
+    return html`
+      <h5>
+        <a href="#/${this.model.id}">
+          <span property="id"></span>
+        </a>
+        <ul property="rdfs:label"><li></li></ul>
+      </h5>
+    `;
+  }
+}
