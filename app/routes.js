0fd913154a73eb0c2b2e757db559cc333af45f92
Roman Karpov <roman.karpov@gmail.com>
2023-04-17 12:18:49 +0300

    app example upd

--- app/routes.js
+++ app/routes.js
@@ -2,23 +2,20 @@ import {Model, Router} from '../src/index.js';
 import './AppComponent.js';
 import './SettingsComponent.js';
 
-const present = (component) => {
-  const main = document.querySelector('main');
-  main.innerHTML = '';
-  main.append(component);
-};
-
-const router = new Router();
-
 const tagDictionary = {
   app: 'veda-app',
   settings: 'veda-settings',
 };
+
+const main = document.querySelector('main');
+
+const router = new Router();
 
 router.add('#/:comp/:id', (comp, id) => {
   const model = new Model(id);
   const tag = tagDictionary[comp];
   const component = document.createElement(tag);
   component.model = model;
-  present(component);
+  main.innerHTML = '';
+  main.append(component);
 });
