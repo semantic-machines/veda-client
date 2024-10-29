a203bad49cee872fd33a4b868d9eb892473e6b90
Roman Karpov <roman.karpov@gmail.com>
2024-10-29 19:34:32 +0300

    export subscription

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export {default as Backend} from './Backend.js';
+export {default as Subscription} from './Subscription.js';
 export {default as Emitter} from './Emitter.js';
 export {default as Observable} from './Observable.js';
 export {default as Model} from './Model.js';
