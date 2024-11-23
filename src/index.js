e12aca1a173d20dcfc21c3c0c5067cb0ef7f3f36
Roman Karpov <roman.karpov@gmail.com>
2024-11-23 08:33:11 +0300

    safe fn to sanitize user input

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ export {default as Emitter} from './Emitter.js';
 export {default as Observable} from './Observable.js';
 export {default as Model} from './Model.js';
 export {default as BaseModel} from './BaseModel.js';
-export {default as Component, html} from './components/Component.js';
+export {default as Component, html, safe} from './components/Component.js';
 export {default as Router} from './Router.js';
 export * from './Util.js';
